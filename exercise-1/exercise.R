dbfad2c3de14ef440e360dd18b4dc2d7018e929a
Yubo Wang <yubow2@uw.edu>
2016-10-11 20:58:45 -0700

    Tried the challenge question for exercise-1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,27 @@
 
 
 # Create a vector of everything you ate for breakfast
-my.breakfast <- list("fried rice", "bacon")
+my.breakfast <- c("fried rice", "bacon")
 
 # Create a vector of everything you ate for lunch
-my.lunch <- list("sausage", "eggs", "onions")
+my.lunch <- c("sausage", "eggs", "onions")
 
 # Create a list `meals` that has contains your breakfast and lunch
-meals <- list(breakfast.meal=my.breakfast, lunch.meal=my.lunch)
+meals <- list(breakfast=my.breakfast, lunch=my.lunch)
 
 # Add a `dinner` index to your `meals` list that has what you plan to eat for dinner
-meals$dinner.meal <- c("Broccoli", "Stir fry")
+meals$dinner <- c("Broccoli", "Stir fry")
 
 # Extract your 'dinner' element from your list and save it in a vector called 'dinner'
-dinner <- meals[["dinner.meal"]]
+dinner <- meals[["dinner"]]
 
 ### Bonus ### 
 # Create a list that has the number of items you ate for each meal
-
+number <- list(lengths(meals))
 
 # Write a function that adds pizza to every meal
-
+add <- function(pizza) {
+  meals <- lapply()
+}
 
 # Add pizza to every meal!
